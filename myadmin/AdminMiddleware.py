9e30f02862b3b403ccad320041ae479b8e5609bb
17671611495 <819577544@qq.com>
2021-12-23 15:58:22 +0800

    修复bug

--- myadmin/AdminMiddleware.py
+++ myadmin/AdminMiddleware.py
@@ -16,6 +16,19 @@ class AdminLoginMiddleware:
         #检测当前的请求是否登录,如果已经登录则放行,未登录则跳转到登录页
         #获取当前用户的请求路径
         urllist=[reverse('home_yzm'),reverse('home_login')]
+
+        urllist1=[
+            reverse('home_yzm'),
+            reverse('home_login'),
+            reverse('mybook_login'),
+            reverse('mybook_register'),
+            reverse('mybook_exit'),
+            reverse('mybook_ckphone'),
+            reverse('mybook_sendcoke'),
+            reverse('mybook_index'),
+            reverse('mybook_list'),
+            reverse('mybook_info')
+        ]
         #判断是否进入了后台,并且不是登录界面 排除urllist
         if re.match('/myadmin/',request.path) and request.path not in urllist:
 
@@ -23,5 +36,10 @@ class AdminLoginMiddleware:
             if request.session.get('AdminUser','')=='':
                 url=reverse('home_login')
                 return HttpResponse('<script>alert("请先登录");location.href="'+url+'"</script>')
+        elif re.match('/',request.path) and request.path not in urllist1:
+            # 检测session中是否有用户数据
+            if request.session.get('myuser', '') == '':
+                url = reverse('mybook_login')
+                return HttpResponse('<script>alert("请先登录");location.href="' + url + '"</script>')
         response=self.get_response(request)
         return response
